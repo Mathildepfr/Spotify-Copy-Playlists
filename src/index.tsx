31ee15b9a3a98d47d4a7058830d9183478a18a9b
Romain Foncier <romain@apptweak.com>
2022-04-27 11:35:57 +0200

    Add Redux & RTK

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
