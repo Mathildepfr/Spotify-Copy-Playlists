93f887464a95272e8997596cb45dd650cdbbe850
RFoncier <Airboy@users.noreply.github.com>
2022-04-27 17:11:18 +0200

    Cleanup store file

--- src/store/store.ts
+++ src/store/store.ts
@@ -18,5 +18,4 @@ sagaMiddleware.run(rootSaga);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
