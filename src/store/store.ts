456125b9ffbaf7afe3f2a4549e0fdd3a6fe06a72
Mathilde Peiffer <peiffermathilde@gmail.com>
2022-11-03 16:32:07 +0100

    Add existing project files to Git

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,16 @@
 import createSagaMiddleware from "redux-saga";
 
 import { configureStore } from "@reduxjs/toolkit";
+// configureStore: A friendly abstraction over the standard Redux createStore function that
+// adds good defaults to the store setup for a better development experience.
+// https://redux-toolkit.js.org/api/configureStore
 
 import reducers from "./reducers";
 import rootSaga from "./sagas";
 
 // create the saga middleware
+// createSagaMiddleware(options)​: Creates a Redux middleware and connects the Sagas to the Redux Store
+// https://redux-saga.js.org/docs/api/
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
 
